5f3c8f27b99799556b960c4b210817e64b51ac77
Théo Pomies <theo.pomies@outlook.com>
2022-01-24 10:31:26 +0100

    feat: add: argument parser

--- src/args.rs
+++ src/args.rs
@@ -4,18 +4,13 @@ pub enum Commands {
 }
 
 pub struct Arguments {
-    pub n1: u64,
-    pub n2: u64,
-    pub n3: u64,
-    pub n4: u64,
-    pub po: u64,
-    pub pw: u64,
-    pub pc: u64,
-    pub pb: u64,
-    pub ps: u64,
+    pub r0: f64,
+    pub r5: f64,
+    pub r10: f64,
+    pub r15: f64,
+    pub r20: f64,
+    pub n: u64,
 }
-
-const TOTAL_ARGS: usize = 9;
 
 impl Commands {
     pub fn try_from_args() -> Result<Commands, String> {
@@ -23,28 +18,31 @@ impl Commands {
         if args.any(|arg| arg == "-h" || arg == "--help") {
             return Ok(Commands::Help);
         }
-        let args = std::env::args()
-            .skip(1)
+        let mut args = std::env::args().skip(1);
+        let radiuses = {
+            let mut radiuses = vec![];
+            for _ in 0..5 {
+                match args.next().map(|arg| arg.parse::<f64>()) {
+                    Some(Ok(n)) => radiuses.push(n),
+                    Some(Err(e)) => return Err(e.to_string()),
+                    _ => return Err("Invalid argument count.".into()),
+                }
+            }
+            radiuses
+        };
+        let n = args
+            .next()
             .map(|arg| arg.parse::<u64>())
-            .collect::<Result<Vec<_>, _>>();
-        match args {
-            Err(e) => Err(e.to_string()),
-            Ok(args) if args.len() == TOTAL_ARGS => Ok(Commands::Program(Arguments {
-                n1: args[0],
-                n2: args[1],
-                n3: args[2],
-                n4: args[3],
-                po: args[4],
-                pw: args[5],
-                pc: args[6],
-                pb: args[7],
-                ps: args[8],
-            })),
-            Ok(args) => Err(format!(
-                "Invalid arguments count {}, expected {}.",
-                args.len(),
-                TOTAL_ARGS
-            )),
-        }
+            .ok_or("Invalid argument count.".to_owned())?
+            .map_err(|e| e.to_string())?;
+
+        Ok(Commands::Program(Arguments {
+            r0: radiuses[0],
+            r5: radiuses[1],
+            r10: radiuses[2],
+            r15: radiuses[3],
+            r20: radiuses[4],
+            n,
+        }))
     }
 }
